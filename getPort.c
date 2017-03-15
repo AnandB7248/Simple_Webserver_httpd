64f8fa4db6106541c80bf97223ab2bc1a378528d
Anand Batjargal <anandb7248@gmail.com>
2017-03-15 16:56:45 -0700

    Finalized getPort.

--- getPort.c
+++ getPort.c
@@ -4,46 +4,43 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define TRUE 1
-#define FALSE 0
+#define PORT_LOW_BOUND 1024
+#define PORT_UPP_BOUND 65535
 
-unsigned short getPort(int argc, char* argv[])
+int getPort(int argc, char* argv[])
 {
+   int port;
+
    if(argc != 2)
    {
       fprintf(stderr, "usage: httpd [port] \n");
       exit(-1);
    }
 
-   if(!isPosNum(argv[1]))
+   /* Convert command line argument to an integer data type */
+   port = atoi(argv[1]);
+
+   /* If argv[1] could not be converted to a integer data type */
+   if(port == 0) 
    {
-      fprintf(stderr, "Error: [port] must be a positive number \n");  
+      fprintf(stderr, "usage: httpd [port] \n");
+      fprintf(stderr, "Please input an integer value for [port]\n");
+      exit(-1);
+   }
+   /* If port value given is negative */
+   else if(port < 0)
+   {
+      fprintf(stderr, "usage: httpd [port] \n");
+      fprintf(stderr, "Error: [port] must be a positive number\n");  
       exit(-1); 
    }
-    
-   return atoi(argv[1]);
-}
-
-unsigned short isPosNum(char* argument)
-{
-   int length;
-   int i = 0;
-
-   if(argument == NULL)
-      return FALSE;
-   
-   length = strlen(argument);
-
-   if(argument[0] == '-')
+   /* Port should be between 1024 and 65535 */
+   else if((port < PORT_LOW_BOUND) || (port > PORT_UPP_BOUND))
    {
-      return FALSE;
+      fprintf(stderr, "usage: httpd [port] \n");
+      fprintf(stderr, "Error: [port] should be a value between 1024 and 65535\n");
+      exit(-1);
    }
-   
-   for(i = 1; i < length; i++)
-   {
-      if(!isdigit(argument[i]))
-         return FALSE;
-   }
-   
-   return TRUE;
+
+   return port;
 }
--- getPort.h
+++ getPort.h
@@ -1,6 +1,9 @@
 #ifndef GET_PORT_H
 #define GET_PORT_H
 
-unsigned short getPort(int, char**);
-unsigned short isPosNum(char*);
+/* Command line usage: ./http [port] */
+/* port : [1024 : 65535] */
+/* Returns the port value passed in from the command line */
+int getPort(int argc, char** argv);
+
 #endif
